7ea6f1c57f197066ab599e9b47c9d775518495ec
KOMAIL BAIG <M.K.BAIG-2021@hull.ac.uk>
2024-04-26 07:54:18 +0100

    Customer Home complete

--- BlazorBeachBasket/obj/Debug/net8.0/BlazorBeachBasket.AssemblyInfo.cs
+++ BlazorBeachBasket/obj/Debug/net8.0/BlazorBeachBasket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorBeachBasket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e35c47170036f337d65af1c6eef76e9448e54c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d265e91c6716b0a20bbadcdd45d2e7cd27794b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorBeachBasket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorBeachBasket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BlazorBeachBasket/obj/Release/net8.0/BlazorBeachBasket.AssemblyInfo.cs
+++ BlazorBeachBasket/obj/Release/net8.0/BlazorBeachBasket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorBeachBasket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab2c9be927e20ff241892339c2a65a839aac254b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d265e91c6716b0a20bbadcdd45d2e7cd27794b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorBeachBasket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorBeachBasket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

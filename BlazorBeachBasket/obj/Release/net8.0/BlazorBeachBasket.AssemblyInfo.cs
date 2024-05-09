0db72faa58cd63124bac54eb6bba748a440c29fc
KOMAIL BAIG <M.K.BAIG-2021@hull.ac.uk>
2024-05-09 01:54:08 +0100

    working

--- BlazorBeachBasket/obj/Release/net8.0/BlazorBeachBasket.AssemblyInfo.cs
+++ BlazorBeachBasket/obj/Release/net8.0/BlazorBeachBasket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorBeachBasket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+917322f5dd45e48c656b1ec7ea949c4b2f34785e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c064fe007f8aa1f158d97cd0353bc5b68432288")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorBeachBasket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorBeachBasket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
